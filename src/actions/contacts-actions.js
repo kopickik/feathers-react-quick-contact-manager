24bdf7d444b1df225d869712e40be42ed30a0917
Kyle Kopicki <kyle.kopicki@gmail.com>
2019-06-16 00:32:16 -0400

    add update actions

--- src/actions/contacts-actions.js
+++ src/actions/contacts-actions.js
@@ -23,3 +23,19 @@ export function saveContact(contact) {
       payload: client.post('contacts', contact)
     })
 }
+
+export function fetchContact(id) {
+  return dispatch =>
+    dispatch({
+      type: 'FETCH_CONTACT',
+      payload: client.get(`contacts/${id}`)
+    })
+}
+
+export function updateContact(contact) {
+  return dispatch =>
+    dispatch({
+      type: 'UPDATE_CONTACT',
+      payload: client.put(`contacts/${contact._id}`, contact)
+    })
+}
